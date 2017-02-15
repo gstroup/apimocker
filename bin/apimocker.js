c43b93a3d4dd4ad199a98c4499f47e1d03c50a26
Greg Stroup <gregory.stroup@ge.com>
2017-02-15 13:59:08 -0800

    lint fixes

--- bin/apimocker.js
+++ bin/apimocker.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-var pkg = require("../package.json"),
-    commander = require("commander"),
-    ApiMocker = require("../lib/apimocker.js");
+var pkg = require('../package.json'),
+	commander = require('commander'),
+	ApiMocker = require('../lib/apimocker.js');
 
 commander
-    .version(pkg.version)
-    .option("-c, --config <path>", "Path to config.json file.", __dirname + "/../config.json")
-    .option("-q, --quiet", "Disable console logging.")
-    .option("-p, --port <port>", "Port that the http mock server will use. Default is 8888.", "8888")
-    .option("-f, --proxy <proxyURL>", "URL of a real service to proxy to, for endpoints that are not mocked.", false)
-    .option("-i, --intercept <proxyIntercept>", "Path to a module that exports an express-http-proxy intercept function")
-    .parse(process.argv);
+		.version(pkg.version)
+		.option('-c, --config <path>', 'Path to config.json file.', __dirname + '/../config.json')
+		.option('-q, --quiet', 'Disable console logging.')
+		.option('-p, --port <port>', 'Port that the http mock server will use. Default is 8888.', '8888')
+		.option('-f, --proxy <proxyURL>', 'URL of a real service to proxy to, for endpoints that are not mocked.', false)
+		.option('-i, --intercept <proxyIntercept>', 'Path to a module that exports an express-http-proxy intercept function')
+		.parse(process.argv);
 
 var options = {};
 options.port = commander.port;
 options.quiet = !!commander.quiet;
 options.proxyURL = commander.proxy;
-options.proxyIntercept = commander.intercept
+options.proxyIntercept = commander.intercept;
 
-var apiMocker = ApiMocker.createServer(options)
-    .setConfigFile(commander.config)
-    .start();
+ApiMocker.createServer(options)
+		.setConfigFile(commander.config)
+		.start();
